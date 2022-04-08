df382d84652dbba9b5a2d24f38da87a4d9101660
DvdHLee <dvdhyelee@gmail.com>
2022-04-08 15:17:57 -0700

    projects done

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,14 +1,45 @@
-import React from 'react';
+import React from "react";
 
-import './Projects.css';
+import "./Projects.css";
 
-const Projects = props => {
-    return (
-        <div className="projects">
-            <p className="projectstitle">PROJECTS</p>
-            <a href="https://github.com/DvdHLee" title="">Test link</a>
-        </div>
-    );
+const Projects = (props) => {
+  return (
+    <div className="projects">
+      <p className="projectstitle">PROJECTS</p>
+      <div className="projectrow">
+        <a className="projectitem" href="https://github.com/DvdHLee" title="">
+          <img
+            id="mcicon"
+            src="/assets/Icons/mealchewser.png"
+            alt="this is a linkedin icon"
+          />
+        </a>
+        <a className="projectitem" href="https://github.com/DvdHLee" title="">
+          <img
+            className="projecticon"
+            src="/assets/Icons/linkedin.png"
+            alt="this is a linkedin icon"
+          />
+        </a>
+      </div>
+      <div className="projectrow">
+        <a className="projectitem" href="https://github.com/DvdHLee" title="">
+          <img
+            className="projecticon"
+            src="/assets/Icons/linkedin.png"
+            alt="this is a linkedin icon"
+          />
+        </a>
+        <a className="projectitem" href="https://github.com/DvdHLee" title="">
+          <img
+            className="projecticon"
+            src="/assets/Icons/linkedin.png"
+            alt="this is a linkedin icon"
+          />
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
